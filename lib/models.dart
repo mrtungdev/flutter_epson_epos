28f079057c9f99be721bfe371af3d34a08a97f67
Marcus Brasizza <mvbdesenvolvimento@gmail.com>
2022-08-09 01:06:07 -0300

    changes

--- lib/models.dart
+++ lib/models.dart
@@ -1,49 +1,107 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 ///
 /// Printer Model
 ///
 class EpsonPrinterModel {
-  EpsonPrinterModel(
-      {required this.address, this.type, this.model, this.series});
-
   /// Connectivity type: TCP | BT | USB
   String? type;
 
   /// Addrs
-  String address;
+  String? ipAddress;
+  String? bdAddress;
+  String? macAddress;
   String? model;
   String? series;
+  String? target;
+  EpsonPrinterModel({
+    this.type,
+    this.ipAddress,
+    this.bdAddress,
+    this.macAddress,
+    this.model,
+    this.series,
+    this.target,
+  });
 
-  EpsonPrinterModel copyWith(
-          {required String address,
-          String? type,
-          String? model,
-          String? series}) =>
-      EpsonPrinterModel(
-          address: address,
-          type: type ?? this.type,
-          model: model ?? this.model,
-          series: series ?? this.series);
+  EpsonPrinterModel copyWith({
+    String? type,
+    String? ipAddress,
+    String? bdAddress,
+    String? macAddress,
+    String? model,
+    String? series,
+    String? target,
+  }) {
+    return EpsonPrinterModel(
+      type: type ?? this.type,
+      ipAddress: ipAddress ?? this.ipAddress,
+      bdAddress: bdAddress ?? this.bdAddress,
+      macAddress: macAddress ?? this.macAddress,
+      model: model ?? this.model,
+      series: series ?? this.series,
+      target: target ?? this.target,
+    );
+  }
 
-  factory EpsonPrinterModel.fromRawJson(String str) =>
-      EpsonPrinterModel.fromJson(json.decode(str));
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'type': type,
+      'ipAddress': ipAddress,
+      'bdAddress': bdAddress,
+      'macAddress': macAddress,
+      'model': model,
+      'series': series,
+      'target': target,
+    };
+  }
 
-  String toRawJson() => json.encode(toJson());
+  factory EpsonPrinterModel.fromMap(Map<String, dynamic> map) {
+    return EpsonPrinterModel(
+      type: map['type'] != null ? map['type'] as String : null,
+      ipAddress: map['ipAddress'] != null ? map['ipAddress'] as String : null,
+      bdAddress: map['bdAddress'] != null ? map['bdAddress'] as String : null,
+      macAddress:
+          map['macAddress'] != null ? map['macAddress'] as String : null,
+      model: map['model'] != null ? map['model'] as String : null,
+      series: map['series'] != null ? map['series'] as String : null,
+      target: map['target'] != null ? map['target'] as String : null,
+    );
+  }
 
-  factory EpsonPrinterModel.fromJson(Map<String, dynamic> json) =>
-      EpsonPrinterModel(
-          address: json["address"],
-          type: json["type"],
-          model: json["model"],
-          series: json["series"]);
+  String toJson() => json.encode(toMap());
 
-  Map<String, dynamic> toJson() =>
-      {"address": address, "type": type, "model": model, "series": series};
+  factory EpsonPrinterModel.fromJson(String source) =>
+      EpsonPrinterModel.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() {
-    return 'Model: $model Addr: $address Series: $series Port: $type';
+    return 'EpsonPrinterModel(type: $type, ipAddress: $ipAddress, bdAddress: $bdAddress, macAddress: $macAddress, model: $model, series: $series, target: $target)';
+  }
+
+  @override
+  bool operator ==(covariant EpsonPrinterModel other) {
+    if (identical(this, other)) return true;
+
+    return other.type == type &&
+        other.ipAddress == ipAddress &&
+        other.bdAddress == bdAddress &&
+        other.macAddress == macAddress &&
+        other.model == model &&
+        other.series == series &&
+        other.target == target;
+  }
+
+  @override
+  int get hashCode {
+    return type.hashCode ^
+        ipAddress.hashCode ^
+        bdAddress.hashCode ^
+        macAddress.hashCode ^
+        model.hashCode ^
+        series.hashCode ^
+        target.hashCode;
   }
 }
 
